36dcf49a596eaa61e9146499081be72b4726c409
Lazar Stojanović <lakisd98@gmail.com>
2022-09-28 14:23:03 +0200

    Update Available Books solution

--- Availiable Books/app.js
+++ Availiable Books/app.js
@@ -1,26 +1,29 @@
 class Book {
-    constructor(title, author, isbn, numCopies) {
+    constructor(title, author, ISBN, numCopies) {
         this.title = title;
         this.author = author;
-        this.isbn = isbn;
+        this.ISBN = ISBN;
         this.numCopies = numCopies;
-    };
+    }
 
-    get getAvailability() {
+    get availability() {
+        return this.getAvailability();
+    }
+
+    getAvailability() {
         if (this.numCopies === 0) {
             return "Out of stock";
         } else if (this.numCopies < 10) {
             return "Low stock";
-        } else {
-            return "In stock";
-        };
-    };
+        }
+        return "In stock";
+    }
 
     sell(numSold = 1) {
         this.numCopies -= numSold;
-    };
+    }
 
     restock(numCopies = 5) {
         this.numCopies += numCopies;
-    };
+    }
 }
